a9387e3cc8dbb0c3415961ae796c153ed6af5630
davincikab <davidnganganjeri079@gmail.com>
2020-08-21 00:03:28 +0300

    Working with custom list

--- main.js
+++ main.js
@@ -4,7 +4,7 @@ mapboxgl.accessToken = 'pk.eyJ1IjoiYmVubnl0cm92YXRvIiwiYSI6ImNrZDcwdTVwbTE4amEye
 
 var map = new mapboxgl.Map({
     container: 'map',
-    style: 'mapbox://styles/bennytrovato/ckdxg3sfd0s211al15hakjjlf', 
+    style: 'mapbox://styles/mapbox/streets-v11', 
     center: [-96.6062800476281,  40.19020391149792],
     zoom: 3,
 });
--- search.js
+++ search.js
@@ -1,5 +1,6 @@
 var searchBar = document.getElementById('location-bar');
 var result = document.getElementById('result');
+var sideSection = document.getElementById('side-section');
 var filterData;
 var searchData;
 var locationMarker;
@@ -40,10 +41,11 @@ function convertToGeoJson(csvData) {
           console.log(data);
           searchData = data.features;
 
+        createMarkers(data);
         if(isMainPage){
             return;
         }
-          createListItems(data.features);
+        createListItems(data.features);
       }
     );
 }
@@ -61,7 +63,6 @@ function forwardGeocoder(query) {
 
         if(isMainPage) {
             result.innerHTML = '';
-
             return;
         }
 
@@ -83,21 +84,75 @@ function forwardGeocoder(query) {
 
     console.log(filterData);
 
-    if(filterData.length == 0) {
-        result.innerHTML = '<div class="py-3 px-1 text-center"><small class="text-center"><b>No result found</b></small><br>'+
+    if(filterData.length == 0 && !isMainPage) {
+        sideSection.innerHTML = '<div class="empty-result py-3 px-1 text-center"><small class="text-center"><b>No result found</b></small><br>'+
                 '<a class="btn btn-cust mr-3" href="">Add Your Business</a><small> '+
                 'No Partners in Your Area ? </small><a href="" class="">About</a></div>'
         return;
+    } else {
+        result.innerHTML = '<div class="empty-result py-3 px-1 text-center"><small class="text-center"><b>No result found</b></small><br>'+
+            '<a class="btn btn-cust mr-3" href="">Add Your Business</a><small> '+
+            'No Partners in Your Area ? </small><a href="" class="">About</a></div>'
     }
 
     // 
-    createListItems(JSON.parse(JSON.stringify(filterData)));
+    if(isMainPage) {
+        createListGroupItems(JSON.parse(JSON.stringify(filterData)));
+    } else {
+        createListItems(JSON.parse(JSON.stringify(filterData)));
+    }
+    
+}
+
+function createMarkers(businessdata) {
+    businessdata.features.forEach(business => {
+        
+        new mapboxgl.Marker()
+            .setLngLat(business.geometry.coordinates)
+            .addTo(map);
+    });
 }
 
 function createListItems(filterData) {
-    var docFrag = document.createDocumentFragment();
+    let categories = filterData.map(datum => datum.properties.category);
+    categories = [...new Set(categories)];
 
-    // create a list of items
+    sideSection.innerHTML = '';
+    categories.forEach(category => {
+        var docFrag = document.createElement('ul');
+        docFrag.classList.add("list-group");
+        docFrag.classList.add("mb-2");
+
+        // add header section
+        docFrag.innerHTML += "<div class='card-header'>"+category+"</div>";
+        let categoryData = filterData.filter(ft => ft.properties.category == category);
+
+        // create a list of items
+        categoryData.forEach(data => {
+            var list = document.createElement('li');
+            list.className = 'address list-group-item';
+
+            list.setAttribute('data-coord', data.geometry.coordinates);
+            list.setAttribute('data-type', 'city');
+            list.setAttribute('data-title', data.properties.name);
+
+            list.innerHTML = data.properties.name;
+
+            list.addEventListener('click',flyToMarker);
+
+            docFrag.appendChild(list);
+            
+        });
+
+        // add to result
+        sideSection.append(docFrag);
+    });
+
+}
+
+function createListGroupItems(filterData) {
+    let docFrag = document.createDocumentFragment();
+
     filterData.forEach(data => {
         var list = document.createElement('li');
         list.className = 'address list-group-item';
@@ -111,10 +166,9 @@ function createListItems(filterData) {
         list.addEventListener('click',flyToMarker);
 
         docFrag.appendChild(list);
-           
+        
     });
 
-    // add to result
     result.innerHTML = "";
     result.append(docFrag);
 }
@@ -147,3 +201,22 @@ function flyToMarker(e) {
         .setLngLat(coordinates)
         .addTo(map);
 }
+
+class LogoControl {
+    onAdd(map) {
+        this._map = map;
+        this._container = document.createElement('div');
+        this._container.className = 'mapboxgl-ctrl';
+        this._container.innerHTML = "<a class='' href='https://groundstandard.com'><img src='images/gsn.logostamp_blue.png' class='img'></a>";
+        return this._container;
+    }
+    
+    onRemove() {
+        this._container.parentNode.removeChild(this._container);
+        this._map = undefined;
+    }
+}
+
+map.on("load", function(e) {
+    map.addControl(new LogoControl(), "bottom-left")
+});
